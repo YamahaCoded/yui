7a751c65b895891f64b2d1fbd0ca0a533a716eb9
Yamaha Coded <144177603+YamahaCoded@users.noreply.github.com>
2024-01-05 13:58:25 -0300

    Update main.go

--- main.go
+++ main.go
@@ -11,29 +11,29 @@ func main() {
 	repo := "mingw-builds-binaries"
 	substringAsset := "win32-seh-msvcrt"
 
-    if len(os.Args) < 2 {
-        fmt.Println("Usage: yui setup -user=<username> -repo=<repository> -substring=<substring> -destiny=<directory> [-confirm]")
-        os.Exit(1)
-    }
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: yui setup -user=<username> -repo=<repository> -substring=<substring> -destiny=<directory> [-confirm]")
+		os.Exit(1)
+	}
 
-    cmd := os.Args[1]
-    switch cmd {
-		case "setup":
-			latestVersion := utils.LastVersion(user, repo)
-			fmt.Println("Latest Version:", latestVersion)
+	cmd := os.Args[1]
+	switch cmd {
+	case "setup":
+		latestVersion := utils.LastVersion(user, repo)
+		fmt.Println("Latest Version:", latestVersion)
 
-			fmt.Print("Do you want to proceed with the download? (y/n): ")
-			var response string
-			fmt.Scanln(&response)
-			if response != "y" {
-				fmt.Println("Download canceled.")
-				os.Exit(0)
-			} else {
-				utils.DownloadAsset(user, repo, substringAsset)
-			}
+		fmt.Print("Do you want to proceed with the download? (y/n): ")
+		var response string
+		fmt.Scanln(&response)
+		if response != "y" {
+			fmt.Println("Download canceled.")
+			os.Exit(0)
+		} else {
+			utils.DownloadAsset(user, repo, substringAsset)
+		}
 
-		default:
-			fmt.Println("Unknown command:", cmd)
-			os.Exit(1)
-    }
+	default:
+		fmt.Println("Unknown command:", cmd)
+		os.Exit(1)
+	}
 }
